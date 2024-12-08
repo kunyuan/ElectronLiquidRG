7d2eb70ffee11a70624b470a38dc192d0536fd95
flyingwagner <hsh191414@mail.ustc.edu.cn>
2024-12-08 11:30:08 +0800

    add benchmark for ver4 PP channel

--- src/ver4_oneloop.jl
+++ src/ver4_oneloop.jl
@@ -315,3 +315,131 @@ function aLambda_thetaphi_avg(para; kamp=[para.kF,], kamp2=kamp, n=[0, 0, 0, 0],
     end
     return ver4, result
 end
+
+#Particle-particle diagram, order = 2
+# sign = (-1)^order = 1
+#
+# kamp,up   kamp2,down
+#t2 |-- KO ---| t2/t3
+#   |         |
+# up^         ^down
+#   |         |
+# 0 |-- KO ---| 0/t1
+#kamp,up  kamp2,down
+
+function _PP_ver4(idx, vars, config)
+    K, T, Ang, Phi, ExtKidx = vars
+    para, kgrid, n = config.userdata
+    k = K[1]
+    dim, β, me, λ, μ, e0, ϵ0 = para.dim, para.β, para.me, para.mass2, para.μ, para.e0, para.ϵ0
+    
+    τ1, τ2, τ3, τ4 = T[1], T[2], T[3], T[4]
+    # θ = 2.533
+    # φ = 0.0
+    
+    θ = Ang[1]
+    φ = Phi[1]
+    extK1 = [sin(0.5θ), cos(0.5θ), 0] * kgrid[ExtKidx[1]]
+    extK2 = [-sin(0.5θ), cos(0.5θ), 0] * kgrid[ExtKidx[1]]
+    extK3 = [sin(0.5θ)*cos(φ),cos(0.5θ),sin(0.5θ)*sin(φ)] * kgrid[ExtKidx[1]]
+
+    k1 = extK1 + extK2 - k
+    
+    # println(q)
+    ϵ = dot(k, k) / (2me) - μ
+    G1 = ElectronLiquid.Propagator.green_derive(τ3 - τ1, ϵ, β, 0)
+    ϵ = dot(k1, k1) / (2me) - μ
+    G2dd = ElectronLiquid.Propagator.green_derive(τ4 - τ2, ϵ, β, 0)
+    G2sd = ElectronLiquid.Propagator.green_derive(τ4 - τ1, ϵ, β, 0)
+    G2ds = ElectronLiquid.Propagator.green_derive(τ3 - τ2, ϵ, β, 0)
+    G2ss = ElectronLiquid.Propagator.green_derive(τ3 - τ1, ϵ, β, 0)
+    q = extK1 - k
+    qabs = sqrt(dot(q, q))
+    V1d = ElectronLiquid.interactionDynamic(para, qabs, τ1, τ2)
+    V1s = ElectronLiquid.interactionStatic(para, qabs, τ1, τ2)
+
+    q = k - extK3
+    qabs = sqrt(dot(q, q))
+    V2d = ElectronLiquid.interactionDynamic(para, qabs, τ3, τ4)
+    V2s = ElectronLiquid.interactionStatic(para, qabs, τ3, τ4)
+    l1 = n[1]
+    l2 = n[2]
+    l3 = n[3]
+    factor = 1.0 / (2π)^3
+    factor_legend = sin(θ) / (4π)
+    fdd = phase((τ1, τ3, τ2, τ4), l1, l2, l3, β) * factor * factor_legend
+    fsd = phase((τ1, τ3, τ1, τ4), l1, l2, l3, β) * factor * factor_legend
+    fds = phase((τ1, τ3, τ2, τ3), l1, l2, l3, β) * factor * factor_legend
+    fss = phase((τ1, τ3, τ1, τ3), l1, l2, l3, β) * factor * factor_legend
+    return G1 * (G2dd *V1d*V2d*fdd + G2ds *V1d*V2s*fds + G2sd *V1s*V2d*fsd + G2ss *V1s*V2s*fss) *para.NF
+end
+
+function _measure_ver4(idx, vars, obs, weight, config) # for the mcmc algorithm
+    # K, T, N, Ang, ExtKidx = vars
+    # ki = ExtKidx[1]
+    ki = vars[5][1]
+    obs[idx][ki] += weight
+end
+
+function aLambda_benchmark(para::ParaMC;
+    kamp=[para.kF],
+    n=[0, 0, 0], 
+    neval=1e6,
+    config=nothing,
+    integrand=_PP_ver4,
+    filename=nothing,
+    kwargs...
+)
+    UEG.MCinitialize!(para, false)
+    
+    kF = para.kF
+    kgrid = kamp
+    K = MCIntegration.FermiK(3, kF, 0.2 * kF, 10.0 * kF)
+    T = MCIntegration.Continuous(0.0, para.β, offset=1)
+    T.data[1] = 0.0
+    Ang = MCIntegration.Continuous(0.0, π)
+    Phi = MCIntegration.Continuous(0.0, 2π)
+    ExtKidx = MCIntegration.Discrete(1, length(kgrid)) 
+    
+    dof = [[1,3,1,1,1],]
+    obs = [zeros(ComplexF64, length(kgrid)),]
+
+    if isnothing(config)
+        config = Configuration(;
+            var=(K, T, Ang, Phi, ExtKidx),
+            dof=dof,
+            type=ComplexF64, # type of the integrand
+            obs=obs,
+            userdata=(para, kgrid, n)
+        )
+    end
+
+    result = integrate(integrand; measure=_measure_ver4, config=config, solver=:mcmc, neval=neval, kwargs...)
+    
+    if isnothing(result) == false
+        avg, std = result.mean[1], result.stdev[1]
+        r = measurement.(real(avg), real(std))
+        i = measurement.(imag(avg), imag(std))
+        # ver3 = Complex.(r, i)
+        ver4 = r
+        for (ki, k) in enumerate(kamp)
+            println("k=$(k/para.kF)kF\t ver4=$(ver4[ki])\n")
+        end
+        # println("ver3=$(ver3)")
+        # println("filename=$(filename)")
+        if isnothing(filename) == false
+            jldopen(filename, "a+") do f
+                key = "$(UEG.short(para))"
+                if haskey(f, key)
+                    @warn("replacing existing data for $key")
+                    delete!(f, key)
+                end
+                f[key] = (kamp, ver4)
+            end
+        end
+
+        return ver4, result
+    else
+        return nothing, nothing
+    end
+end
